a00cfc857a520f2361b11c9523ef20a0cf2b2bb1
Lucas Yuki <lucasyuki@yahoo.com.br>
2025-07-13 15:11:24 -0300

    epl: restultado ok?
    Signed-off-by: Lucas Yuki <lucasyuki@yahoo.com.br>

--- src/epl/test/simple01-simulated_annealing.tcl
+++ src/epl/test/simple01-simulated_annealing.tcl
@@ -3,7 +3,7 @@ set test_name simple01-random
 read_lef ./Nangate45/Nangate45.lef
 read_def ./designs/simple01.def
 
-epl::eplace_simulated_anealing -wait_iterations 10000 -alpha 0.999999 -initial_T 1e3 -density 1.2
+epl::eplace_simulated_anealing -wait_iterations 10000 -alpha 0.9999995 -initial_T 1e3 -density 1.2
 # fazer a densidade começar a alta e ir baixando
 #epl::eplace_simulated_anealing -wait_iterations 10000 -alpha 0.999995 -initial_T 1e3 -simple 
 detailed_placement
